d8545f04eaf336c27bd09ea4d0079b7274513327
MarlonDB-003 <marlondeividmd@gmail.com>
2026-06-04 11:45:13 -0400

    refactor: updates no portifólio

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Engenheiro de Software', 'Desenvolvedor de Software'],
+    strings: ['BackEnd Developer', 'FrontEnd Developer', 'Software Engineer', 'Desenvolvedor de Software'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
